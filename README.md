fd21be53b7a72ab467fbc9005a709e71c9ccb09e
NicolisAlessandro <19131@studenti.marconiverona.edu.it>
2022-03-22 10:16:43 +0100

    readme e movimento

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TheAdventuresGrid
-Group project
+
+## descrizione 
+
+è un gioco openworld, dove si può interagire con gli NPC e la natura, all'interno di un grande mondo fatto da diversi tipi di terreni e habitat da esplorare. In questo mondo il sole sorge e tramonta, si raccolgono materiali e si costruiscono utensili.
+
+Ogni tanto ci sono animali da poter cacciare. A seconda della modalità in cui si sta giocando, puoi scegliere la difficoltà dei nemici.
+
+
+## regolamento
+
+Sopravvivi più che puoi.
+
+
+
+## Group project
+### member
+
+Demartini, Nicolis
--- src/Scacchi.java
+++ src/Scacchi.java
@@ -43,4 +43,92 @@ public class Scacchi {
 
     }
 
+    public String spostaSu(int id, short s){
+        for (int i = 0; i < scacchiera.getRowCount(); i++) {
+          for (int j = 0; j < scacchiera.getColumnCount(); j++) {
+            if(!scacchiera[i][j].getStato() && id == scacchiera[i][j].getNumPedina() && s == scacchiera[i][j].getColorePedina()){
+              try{
+                if(scacchiera[i-1][j].getStato() | scacchiera[i-1][j].getColorePedina() != s){
+                  scacchiera[i-1][j].removePedina();
+                  scacchiera[i-1][j].insertPedina(scacchiera[i][j].getPedina(), scacchiera[i][j].getNumPedina());
+                  scacchiera[i][j].removePedina();
+                  return "Ok";
+                } else if(scacchiera[i-1][j].getColorePedina() == s){
+                  return "Mossa non possibile colore uguale";
+                }
+              }catch(ArrayIndexOutOfBoundsException e){
+                return "Mossa non possiblie raggiunti i limiti della scacchiera";
+              }
+            }
+          }
+        }
+        return "Non trovata pedina";
+      }
+      public String spostaGiu(int id, short s){
+        for (int i = 0; i < SIZE; i++) {
+          for (int j = 0; j < SIZE; j++) {
+            if(!scacchiera[i][j].getStato() && id == scacchiera[i][j].getNumPedina() && s == scacchiera[i][j].getColorePedina()){
+              try{
+                if(scacchiera[i+1][j].getStato() | scacchiera[i+1][j].getColorePedina() != s){
+                  scacchiera[i+1][j].removePedina();
+                  scacchiera[i+1][j].insertPedina(scacchiera[i][j].getPedina(), scacchiera[i][j].getNumPedina());
+                  scacchiera[i][j].removePedina();
+                  return "Ok";
+                } else if(scacchiera[i+1][j].getColorePedina() == s){
+                  return "Mossa non possibile colore uguale";
+                }
+              }catch(ArrayIndexOutOfBoundsException e){
+                return "Mossa non possiblie raggiunti i limiti della scacchiera";
+              }
+            }
+          }
+        }
+        return "Non trovata pedina";
+      }
+    
+      public String spostaDx(int id, short s){
+        for (int i = 0; i < SIZE; i++) {
+          for (int j = 0; j < SIZE; j++) {
+            if(!scacchiera[i][j].getStato() && id == scacchiera[i][j].getNumPedina() && s == scacchiera[i][j].getColorePedina()){
+              try{
+                if(scacchiera[i][j+1].getStato() | scacchiera[i][j+1].getColorePedina() != s){
+                  scacchiera[i][j+1].removePedina();
+                  scacchiera[i][j+1].insertPedina(scacchiera[i][j].getPedina(), scacchiera[i][j].getNumPedina());
+                  scacchiera[i][j].removePedina();
+                  return "Ok";
+                } else if(scacchiera[i][j+1].getColorePedina() == s){
+                  return "Mossa non possibile colore uguale";
+                }
+              }catch(ArrayIndexOutOfBoundsException e){
+                return "Mossa non possiblie raggiunti i limiti della scacchiera";
+              }
+            }
+          }
+        }
+        return "Non trovata pedina";
+      }
+    
+      public String spostaSx(int id, short s){
+        for (int i = 0; i < SIZE; i++) {
+          for (int j = 0; j < SIZE; j++) {
+            if(!scacchiera[i][j].getStato() && id == scacchiera[i][j].getNumPedina() && s == scacchiera[i][j].getColorePedina()){
+              try{
+                if(scacchiera[i][j-1].getStato() | scacchiera[i][j-1].getColorePedina() != s){
+                  scacchiera[i][j-1].removePedina();
+                  scacchiera[i][j-1].insertPedina(scacchiera[i][j].getPedina(), scacchiera[i][j].getNumPedina());
+                  scacchiera[i][j].removePedina();
+                  return "Ok";
+                } else if(scacchiera[i][j-1].getColorePedina() == s){
+                  return "Mossa non possibile colore uguale";
+                }
+              }catch(ArrayIndexOutOfBoundsException e){
+                return "Mossa non possiblie raggiunti i limiti della scacchiera";
+              }
+            }
+          }
+        }
+        return "Non trovata pedina";
+      }
+    
+
 }
