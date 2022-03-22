b22e7b9b38b667d5ad81a7e3adcf5690bffe06f3
NicolisAlessandro <19131@studenti.marconiverona.edu.it>
2022-03-22 21:36:00 +0100

    Prova oggetto

--- src/Albero.java
+++ src/Albero.java
@@ -1,3 +1,24 @@
 public class Albero {
+    private String nome;
+    private Bacca b;
+    private int nb;
+
+    public void raccogliBacche(Bacca a, int nb) {
+        a.add()
+    }
+
+    public Albero(){
+        nome = new String("albero");
+        b = new Bacca();
+    }
 
 }
+
+interface AzioneAlbero{
+    public void raccogliBacche();
+
+}
+
+
+
+
---
+++ src/Bacca.java
@@ -0,0 +1,14 @@
+public class Bacca{
+    private String oggetto;
+    private String nome;
+
+
+    public Bacca(){
+        oggetto = null;
+        nome = null;
+    }
+
+    public Bacca(String n){
+        nome = n;
+    }
+}
